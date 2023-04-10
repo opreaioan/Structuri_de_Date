37f8c7937315a4f05fe003bf217100838e4bc9d9
opreaioan <33578191+opreaioan@users.noreply.github.com>
2023-04-10 23:59:35 +0300

    Update Problema 3.cpp

--- Tema 3/Problema 3.cpp
+++ Tema 3/Problema 3.cpp
@@ -4,7 +4,54 @@ scrie o functie care are ca parametru cele doua siruri si care returneaza true
 daca al doilea este o permutare a primului si false altfel. Implementati folosind
 unordered_set din stl. (1p)
 */
+/*REZOLVARE CU unordered_set - GOOD ONE! :))) */ 
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <unordered_set>
 
+typedef std::unordered_set<char> set;
+
+void citireSiruri(std::string& s1, std::string& s2);
+bool estePermutare(std::string s1, std::string s2);
+
+int main()
+{
+	std::string S1, S2;
+	citireSiruri(S1, S2);
+
+	if (estePermutare(S1, S2))
+		std::cout << "Este permutare!";
+	else std::cout << "Nu este permutare!";
+	return 0;
+}
+void citireSiruri(std::string& s1, std::string& s2) {
+	std::ifstream f("siruriPb3.txt");
+	if (!std::getline(f, s1)) {
+		std::cout << "Eroare la deschidere sau citire din fisier!";
+		exit(1);
+	}
+	getline(f, s2);
+	f.close();
+}
+bool estePermutare(std::string s1, std::string s2) {
+	if (s1.length() != s2.length()) {
+		return 0;
+	}
+	set setSir1, setSir2;
+	int sumSir1 = 0, sumSir2 = 0;
+	for (char c : s1) {
+		setSir1.insert(c);
+		sumSir1 += (int)c;
+	}
+	for (char c : s2) {
+		setSir2.insert(c);
+		sumSir2 += (int)c;
+	}
+	return ((setSir1 == setSir2) && (sumSir1 == sumSir2));
+}
+
+/*REZOLVARE CU unordered_map - just for fun!
 #include <iostream>
 #include <unordered_map>
 #include <string>
@@ -45,4 +92,4 @@ int main()
 	citireSiruri(S1, S2);
 	if (estePermutare(S1, S2)) std::cout << "Este permutare!";
 	else std::cout << "Nu este permutare!";
-}
+}*/
