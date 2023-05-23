77bbaf9e09b41696bd7d1074664abfb2b26cb31d
Oprea Ioan <oprea.ioan@ymail.com>
2023-05-23 22:51:48 +0300

    Update Problema 01.cpp

--- Tema 5/Problema 01.cpp
+++ Tema 5/Problema 01.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
-#include <stack>
+#include <vector>
 #include <stack>
 #include <queue>
-#include <unordered_map>
-#include <vector>
+
 
 class NOD {
 public:
@@ -22,36 +21,59 @@ public:
 		root = new NOD(key);
 	}
 	void INSERT(int key) {
-		NOD* curent = root;
-		while (curent != nullptr) {
-			if (key < curent->key) {
-				if (curent->st != nullptr)
-					curent = curent->st;
-				else {
-					curent->st = new NOD(key);
-					curent->st->p = curent;
-					return;
-				}
-			}
-			else
-				if (key > curent->key) {
-					if (curent->dr != nullptr)
-						curent = curent->dr;
+		if (!root)
+			root = new NOD(key);
+		else {
+			NOD* curent = root;
+			while (curent != nullptr) {
+				if (key < curent->key) {
+					if (curent->st != nullptr)
+						curent = curent->st;
 					else {
-						curent->dr = new NOD(key);
-						curent->dr->p = curent;
+						curent->st = new NOD(key);
+						curent->st->p = curent;
 						return;
 					}
 				}
 				else
-					return;
+					if (key > curent->key) {
+						if (curent->dr != nullptr)
+							curent = curent->dr;
+						else {
+							curent->dr = new NOD(key);
+							curent->dr->p = curent;
+							return;
+						}
+					}
+					else
+						return;
+			}
 		}
 	}
+	NOD* MAXIM(NOD* subTreeRoot) {
+		if (!subTreeRoot) {
+			std::cout << "INVALID INPUT!\n";
+			return nullptr;
+		}
+		else {
+			NOD* curent = subTreeRoot;
+			while (curent->dr) {
+				curent = curent->dr;
+			}
+			std::cout << "MAX is " << curent->key << "\n";
+			return curent;
+		}
+
+	}
+	NOD* FIND(int key) {
+		NOD* curent = root;
+
+	}
 	void PRINT_TREE(int opt) {
-		std::cout
+		/*std::cout
 			<< "Introduceti optiunea: 1 - PREORDER, "
 			<< "2 - INORDER, 3 - POSTORDER, 4 - BFS\n"
-			<< "\t>>";
+			<< "\t>>";*/
 
 		switch (opt) {
 		case 1: {
@@ -74,7 +96,6 @@ public:
 			BFS();
 			break;
 		}
-
 		}
 	}
 	void PREORDER() {
@@ -132,9 +153,10 @@ public:
 			if (curent->dr != nullptr)
 				stiva.push(curent->dr);
 		}
-		std::cout << "\n";
 		for (int i = rezultat.size() - 1; i >= 0; i--)
 			std::cout << rezultat[i]->key << " ";
+		std::cout << "\n";
+
 	}
 	void BFS() {
 		if (root == nullptr) {
@@ -142,7 +164,6 @@ public:
 			return;
 		}
 		std::queue<NOD*> coada;
-		std::unordered_map<int, std::vector<NOD*>> elementePeNiveluri;
 		coada.push(root);
 		int nivel = 0;
 
@@ -150,7 +171,6 @@ public:
 			int n = coada.size();
 			while (n > 0) {
 				NOD* curent = coada.front();
-				elementePeNiveluri[nivel].push_back(curent);
 				std::cout << curent->key << " ";
 				coada.pop();
 				if (curent->st != nullptr)
@@ -162,27 +182,54 @@ public:
 			nivel++;
 			std::cout << "\n";
 		}
-		/*for (auto& it : elementePeNiveluri) {
-			std::vector<NOD*>& elemente = it.second;
-			for (auto& it2 : elemente)
-				std::cout << it2->key << " ";
-			std::cout << "\n";
-		}*/
 	}
 };
 
 int main() {
 	SearchTree T;
-	T.INSERT(10);
-	T.INSERT(5);
-	T.INSERT(15);
-	T.INSERT(3);
-	T.INSERT(7);
-	T.INSERT(12);
-	T.INSERT(17);
-	T.INSERT(1);
-	T.INSERT(4);
 	int opt;
+	std::cout
 
-	T.PRINT_TREE(opt);
+		<< "1. INSERT\n"
+		<< "2. MAXIM\n"
+		<< "3. MINIM\n"
+		<< "4. SUCCESOR\n"
+		<< "5. PREDECESOR\n"
+		<< "6. FIND\n"
+		<< "7. DELETE\n"
+		<< "8. ERASE\n"
+		<< "9. PRINT TREE\n"
+		<< "10. CONSTRUCT\n"
+		<< "11. EMPTY\n"
+		<< "12. CLEAR\n"
+		<< "Option >> ";
+
+	std::cin >> opt;
+	switch (opt) {
+	case 1: {
+		std::cout << "Insert key >> ";
+		int key;
+		std::cin >> key;
+		T.INSERT(key);
+		break;
+	}
+	case 2: {
+		std::cout << "Subtree root >> ";
+		int subRootKey;
+		std::cin >> subRootKey;
+		T.MAXIM(T.FIND(subRootKey));
+	}
+	}
+
+	/*
+			 10
+		   /    \
+		  5     15
+		 / \    / \
+		3   7  12  17
+	   / \
+	  1   4
+
+	*/
+
 }
