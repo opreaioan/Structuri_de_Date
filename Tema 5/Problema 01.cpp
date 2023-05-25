85c584d246712ac51ba4e5d1ee34cbebb12bb746
Oprea Ioan <oprea.ioan@ymail.com>
2023-05-25 08:57:00 +0300

    Update Problema 01.cpp

--- Tema 5/Problema 01.cpp
+++ Tema 5/Problema 01.cpp
@@ -14,6 +14,92 @@ public:
 		:key(key), st(nullptr), dr(nullptr), p(nullptr) {}
 };
 class SearchTree {
+private:
+	void PREORDER() {
+		if (root == nullptr) {
+			std::cout << " Arbore nul\n";
+			return;
+		}
+		std::stack<NOD*> stiva;
+		stiva.push(this->root);
+		while (!stiva.empty()) {
+			NOD* curent = stiva.top();
+			stiva.pop();
+			std::cout << " " << curent->key << " ";
+			if (curent->dr != nullptr)
+				stiva.push(curent->dr);
+			if (curent->st != nullptr)
+				stiva.push(curent->st);
+		}
+		std::cout << "\n";
+	}
+	void INORDER() {
+		if (root == nullptr) {
+			std::cout << " Arbore nul\n";
+			return;
+		}
+		std::stack<NOD*> stiva;
+		NOD* curent = this->root;
+		while (curent != nullptr || !stiva.empty()) {
+			while (curent != nullptr) {
+				stiva.push(curent);
+				curent = curent->st;
+			}
+			curent = stiva.top();
+			stiva.pop();
+			std::cout << " " << curent->key << " ";
+			curent = curent->dr;
+		}
+		std::cout << "\n";
+	}
+	void POSTORDER() {
+		if (root == nullptr) {
+			std::cout << " Arbore nul\n";
+			return;
+		}
+		std::stack<NOD*> stiva;
+		std::vector<NOD*> rezultat;
+		int poz = 0;
+		stiva.push(this->root);
+		while (!stiva.empty()) {
+			NOD* curent = stiva.top();
+			stiva.pop();
+			rezultat.emplace_back(curent);
+			if (curent->st != nullptr)
+				stiva.push(curent->st);
+			if (curent->dr != nullptr)
+				stiva.push(curent->dr);
+		}
+		for (int i = rezultat.size() - 1; i >= 0; i--)
+			std::cout << " " << rezultat[i]->key << " ";
+		std::cout << "\n";
+
+	}
+	void BFS() {
+		if (root == nullptr) {
+			std::cout << " Arbore nul\n";
+			return;
+		}
+		std::queue<NOD*> coada;
+		coada.push(root);
+		int nivel = 0;
+
+		while (!coada.empty()) {
+			int n = coada.size();
+			while (n > 0) {
+				NOD* curent = coada.front();
+				std::cout << " " << curent->key << " ";
+				coada.pop();
+				if (curent->st != nullptr)
+					coada.push(curent->st);
+				if (curent->dr != nullptr)
+					coada.push(curent->dr);
+				n--;
+			}
+			nivel++;
+			std::cout << "\n";
+		}
+	}
 public:
 	NOD* root;
 	SearchTree() :root(nullptr) {}
@@ -175,6 +261,7 @@ public:
 
 	}
 	void PRINT_TREE(int opt) {
+
 		switch (opt) {
 		case 1: {
 			std::cout << " PREORDER: ";
@@ -198,91 +285,6 @@ public:
 		}
 		}
 	}
-	void PREORDER() {
-		if (root == nullptr) {
-			std::cout << " Arbore nul\n";
-			return;
-		}
-		std::stack<NOD*> stiva;
-		stiva.push(this->root);
-		while (!stiva.empty()) {
-			NOD* curent = stiva.top();
-			stiva.pop();
-			std::cout << curent->key << " ";
-			if (curent->dr != nullptr)
-				stiva.push(curent->dr);
-			if (curent->st != nullptr)
-				stiva.push(curent->st);
-		}
-		std::cout << "\n";
-	}
-	void INORDER() {
-		if (root == nullptr) {
-			std::cout << " Arbore nul\n";
-			return;
-		}
-		std::stack<NOD*> stiva;
-		NOD* curent = this->root;
-		while (curent != nullptr || !stiva.empty()) {
-			while (curent != nullptr) {
-				stiva.push(curent);
-				curent = curent->st;
-			}
-			curent = stiva.top();
-			stiva.pop();
-			std::cout << curent->key << " ";
-			curent = curent->dr;
-		}
-		std::cout << "\n";
-	}
-	void POSTORDER() {
-		if (root == nullptr) {
-			std::cout << " Arbore nul\n";
-			return;
-		}
-		std::stack<NOD*> stiva;
-		std::vector<NOD*> rezultat;
-		int poz = 0;
-		stiva.push(this->root);
-		while (!stiva.empty()) {
-			NOD* curent = stiva.top();
-			stiva.pop();
-			rezultat.emplace_back(curent);
-			if (curent->st != nullptr)
-				stiva.push(curent->st);
-			if (curent->dr != nullptr)
-				stiva.push(curent->dr);
-		}
-		for (int i = rezultat.size() - 1; i >= 0; i--)
-			std::cout << rezultat[i]->key << " ";
-		std::cout << "\n";
-
-	}
-	void BFS() {
-		if (root == nullptr) {
-			std::cout << " Arbore nul\n";
-			return;
-		}
-		std::queue<NOD*> coada;
-		coada.push(root);
-		int nivel = 0;
-
-		while (!coada.empty()) {
-			int n = coada.size();
-			while (n > 0) {
-				NOD* curent = coada.front();
-				std::cout << curent->key << " ";
-				coada.pop();
-				if (curent->st != nullptr)
-					coada.push(curent->st);
-				if (curent->dr != nullptr)
-					coada.push(curent->dr);
-				n--;
-			}
-			nivel++;
-			std::cout << "\n";
-		}
-	}
 };
 
 int main() {
@@ -294,21 +296,21 @@ int main() {
 	int opt;
 	do {
 		std::cout
-			<< " |-----------------------|\n"
-			<< " |   1.INSERT            |\n"
-			<< " |   2.MAXIM             |\n"
-			<< " |   3.MINIM             |\n"
-			<< " |   4.SUCCESOR          |\n"
-			<< " |   5.PREDECESOR        |\n"
-			<< " |   6.FIND              |\n"
-			<< " |   7.DELETE            |\n"
-			<< " |   8.ERASE             |\n"
-			<< " |   9.PRINT TREE        |\n"
-			<< " |   10.CONSTRUCT        |\n"
-			<< " |   11.EMPTY            |\n"
-			<< " |   12.CLEAR            |\n"
-			<< " |   0.EXIT              |\n"
-			<< " |-----------------------|\n\n";
+			<< " |---------------------|\n"
+			<< " |    0 - EXIT         |\n"
+			<< " |    1 - INSERT       |\n"
+			<< " |    2 - MAXIM        |\n"
+			<< " |    3 - MINIM        |\n"
+			<< " |    4 - SUCCESOR     |\n"
+			<< " |    5 - PREDECESOR   |\n"
+			<< " |    6 - FIND         |\n"
+			<< " |    7 - DELETE       |\n"
+			<< " |    8 - ERASE        |\n"
+			<< " |    9 - PRINT TREE   |\n"
+			<< " |   10 - CONSTRUCT    |\n"
+			<< " |   11 - EMPTY        |\n"
+			<< " |   12 - CLEAR        |\n"
+			<< " |---------------------|\n\n";
 		std::cout << " Option >> ";
 		std::cin >> opt;
 		switch (opt) {
@@ -326,7 +328,7 @@ int main() {
 			int subRootKey;
 			std::cin >> subRootKey;
 			if (T.MAXIM(T.FIND(subRootKey)))
-				std::cout << T.MAXIM(T.FIND(subRootKey))->key << "\n";
+				std::cout << " " << T.MAXIM(T.FIND(subRootKey))->key << "\n";
 			break;
 		}
 			  /*3. MINIM*/
@@ -335,7 +337,7 @@ int main() {
 			int subRootKey;
 			std::cin >> subRootKey;
 			if (T.MINIM(T.FIND(subRootKey)))
-				std::cout << T.MINIM(T.FIND(subRootKey))->key << "\n";
+				std::cout << " " << T.MINIM(T.FIND(subRootKey))->key << "\n";
 			break;
 		}
 			  /*4. SUCCESOR*/
@@ -344,7 +346,7 @@ int main() {
 			int key;
 			std::cin >> key;
 			if (T.SUCCESSOR(T.FIND(key)))
-				std::cout << T.SUCCESSOR(T.FIND(key))->key << "\n";
+				std::cout << " " << T.SUCCESSOR(T.FIND(key))->key << "\n";
 			break;
 		}
 			  /*5. PREDECESOR*/
@@ -353,19 +355,43 @@ int main() {
 			int key;
 			std::cin >> key;
 			if (T.PREDECESSOR(T.FIND(key)))
-				std::cout << T.PREDECESSOR(T.FIND(key))->key << "\n";
+				std::cout << " " << T.PREDECESSOR(T.FIND(key))->key << "\n";
 			break;
 		}
+			  /*6.FIND*/
 		case 6: {
 			if (T.FIND(5)) {
 				NOD* x = T.FIND(5);
-				std::cout << x->key << "\n";
+				std::cout << " " << x->key << "\n";
 			}
 			else
 				std::cout << " Negasit\n";
 			break;
 		}
+			  /*7.DELETE*/
+		case 7: {
+			std::cout << " Insert node key >> ";
+			int key;
+			std::cin >> key;
+			T.DELETE(key);
+			break;
+		}
+		case 8: {
 
+		}
+		case 9: {
+			std::cout
+				<< " 1-DFS:Preorder (RSD)\n"
+				<< " 2-DFS:InOrder (SRD)\n"
+				<< " 3-DFS:Postorder (SDR)\n"
+				<< " 4-BFS:Levels\n"
+				<< " 0-Exit"
+				<< " Option >> ";
+			int prtOpt;
+			std::cin >> prtOpt;
+			T.PRINT_TREE(prtOpt);
+
+		}
 		}
 		system("pause");
 		system("cls");
